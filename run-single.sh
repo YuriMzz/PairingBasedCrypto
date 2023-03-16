fd9b89c8a33c92540a3d567f538f2197c0908e9a
YuriMzz <86317421+YuriMzz@users.noreply.github.com>
2023-03-16 21:07:16 +0100

    cleanup

--- run-single.sh
+++ run-single.sh
@@ -7,4 +7,3 @@ make librelic TARGET=zoul BENCH_TIME=1 FUNCTIONS=GPSW \
 RELIC_CMAKE_SCRIPT=/home/user/contiki-ng//presets/relic-cmake-BN-P158.sh \
 main.upload login
 #make BENCH_TIME=1  FUNCTIONS=GPSW  GPSW_SIM=1 GSPW_N_ATTR=2 GPSW_POLICY_STRING='"1 0 2of2"' 
-# /home/user/contiki-ng/relic/src/bn/relic_bn_div.c:145
